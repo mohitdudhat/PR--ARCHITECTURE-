b8edb57e10045ea254cedbb2b3a8b99c548b4101
jaramangukiya <jaramangukiya@gmail.com>
2023-04-03 10:31:52 +0530

    init

---
+++ js/function.js
@@ -0,0 +1,24 @@
+// DOM Element's
+const counters = document.querySelectorAll('.counter');
+
+
+
+for (let n of counters) {
+    const updateCount = () => {
+        const target = +n.getAttribute('data-target');
+        const count = +n.innerText;
+        const divider = 5000;
+        const speed = 50; // 1000 millisecond => 1 second;
+
+        const inc = target / divider;
+
+        if (count < target) {
+            n.innerText = Math.ceil(count + inc);
+            setTimeout(updateCount, speed);
+        } else {
+            n.innerText = target;
+        }
+    };
+
+    updateCount();
+}
